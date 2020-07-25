a9db9bf5d3090054a04dcceecd888b7e15c0961f
TulioAbreu <marco-tulioabreu1@hotmail.com>
2020-07-25 00:57:59 -0300

    Built-in getPage errors

--- src/fetchHtml.ts
+++ src/fetchHtml.ts
@@ -1,16 +1,36 @@
 const nodeFetch: NodeRequire = require('node-fetch');
+import sleep from "./sleep";
 
 interface Page {
     status: string,
     htmlText: string,
 }
 
-async function getPageText(url: string): Promise<Page> {
-    const pageHtml: Response = await nodeFetch(url);
+const MAX_REQUESTS = 10;
+const HTTP_STATUS_OK = 200;
+const SLEEP_TIME = 1000;
 
-    return {
-        status: pageHtml.status.toString(),
-        htmlText: await pageHtml.text(),
+async function getPageText(url: string): Promise<Page | undefined> {
+    let page: Response;
+    let requests = 0;
+
+    do {
+        page = await nodeFetch(url);
+        requests += 1;
+
+        if (page.status !== HTTP_STATUS_OK) {
+            await sleep(SLEEP_TIME);
+        }
+    } while (page.status !== HTTP_STATUS_OK && requests < MAX_REQUESTS);
+
+    if (requests >= MAX_REQUESTS) {
+        console.error("[ERROR] getPageText => Maximum request attempts.");
+        return undefined;
+    } else {
+        return {
+            status: page.status.toString(),
+            htmlText: await page.text(),
+        }
     }
 }
 
