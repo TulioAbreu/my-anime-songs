aa6081e97357515bfd3f0e39c601824060d525eb
TulioAbreu <marco-tulioabreu1@hotmail.com>
2020-07-31 20:41:18 -0300

    refactor(main): separe functions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Prompts from "prompts";
 import Chalk from "chalk";
 import { parseSongToKeyword } from "./modules/keyword-parser";
-import { getUserAnimelist } from "./modules/mal-retriever";
+import { getProfileUrl, getUserAnimeUrlsFromPage } from "./modules/mal-retriever";
 import { getSongList } from "./modules/song-list-retriever";
 import { getYoutubeSongs } from "./youtubeSearch";
 import saveJSON from "./modules/utils/save-json";
+import { getPageText } from "./modules/page-retriever";
 
 async function main() {
     const { username } = await Prompts({
@@ -13,13 +14,16 @@ async function main() {
         message: "What is your MyAnimeList username?",
     });
 
-    const userAnimeList = await getUserAnimelist(username);
-    if (!userAnimeList.length) {
+    const userAnimeListUrl = getProfileUrl(username);
+    const userAnimeListPage = (await getPageText(userAnimeListUrl))?.htmlText;
+    if (!userAnimeListPage) {
         console.error(`❌ ${Chalk.red("ERROR")} User has empty list or does not exist.`);
         return;
     }
 
+    const userAnimeList = getUserAnimeUrlsFromPage(userAnimeListPage);
     const animeSongs = await getSongList(username, userAnimeList);
+
     const youtubeKeywords = animeSongs.map((animeSong: string) => {
         return parseSongToKeyword(animeSong);
     });
